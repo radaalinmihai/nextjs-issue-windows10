06f0592885955d0ab4bf191f43d2138b467517dc
radaalinmihai <radaalinmihai@gmail.com>
2024-02-17 14:22:23 +0200

    Added minimal reproducible code

---
+++ app/some-page/_lib/components/SomeComponent.tsx
@@ -0,0 +1,3 @@
+export default function ExampleComponent() {
+    return <div>I am a component</div>
+}
---
+++ app/some-page/page.tsx
@@ -0,0 +1,10 @@
+import SomeComponent from "./_lib/components/SomeComponent";
+
+export default function SomePage() {
+    return (
+        <>
+        <div>Example page</div>
+        <SomeComponent />
+        </>
+    );
+}
